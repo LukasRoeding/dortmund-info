26e34d5c707a3549664af3452f19d2bf35ccff08
LukasRöding <goskodovesche@gmail.com>
2020-06-13 13:24:27 +0200

    more commentary

--- src/app/bvb/bvb.component.ts
+++ src/app/bvb/bvb.component.ts
@@ -7,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bvb.component.css']
 })
 export class BvbComponent implements OnInit {
+/** an array with all the games the bvb plays in the near future */
 spieltermine=["May 16, 2020","May 23, 2020","May 26, 2020","May 30, 2020","Jun 6, 2020","Jun 13, 2020",
 "Jun 16, 2020","Jun 20, 2020","Jun 27, 2020"]
+/** myDate stores the current date. In the html document myDate gets compared to the dates in the array using the date pipe. */
 myDate = Date.now();
 heute:boolean;
   
